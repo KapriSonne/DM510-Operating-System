adf2bc6e5739093144993155c8f43856ae0f3a22
Jeff Gyldenbrand Skov Joergensen <jegyl16@imada-106310.imada.sdu.dk>
2018-02-07 11:52:33 +0100

    tutorials from net

---
+++ tutorials/c-tut/intro.c
@@ -0,0 +1,9 @@
+// include is the "preprocessor" directive which tells the compiler to put
+// code from the header
+#include <stdio.h>
+int main()
+{
+    printf( "blah.\n" );
+    //getchar();
+    return 0;
+}
