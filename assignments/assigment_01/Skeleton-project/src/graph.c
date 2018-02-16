05573c5b464635fc9cb3164d313814470366bebc
Jeff Gyldenbrand Skov Joergensen <jegyl16@imada-106312.imada.sdu.dk>
2018-02-16 15:37:03 +0100

    mere blah

--- assignments/assigment_01/Skeleton-project/src/graph.c
+++ assignments/assigment_01/Skeleton-project/src/graph.c
@@ -2,42 +2,30 @@
 #include "graph.h"
 #include <stdio.h>
 
-/*
-
-typedef struct graph {
-	int number_vertices;
-	vertex *vertices;
-} graph;
-
-typedef struct vertex {
-	int id;
-	linked_list *out_neighbours;
-	linked_list *in_neighbours;
-} vertex;
-
-*/
-
-
 graph *read_graph(char *filename) {
 	FILE *file = fopen(filename, "r");
 	char *myString = NULL;
-	size_t where_am_I = 0;
+	unsigned int where_am_I = 0;
 
-	getline(&myString, &where_am_I, file);
-	long int sizeOfVertices = strtol(myString, NULL, 10);
+	
 
-	// for debugging purposes.
-	printf("\nNo. of vertices: %li\n-------------------\n",sizeOfVertices);
-
-	for(int i = 0; i < sizeOfVertices; i++){
-		where_am_I++;
-		getline(&myString, &where_am_I, file);
-		long int getVertices = strtol(myString, NULL, 10);
-		printf("%li\n",getVertices);
-	}
-	printf("\n");
+	getline(myString, where_am_I, file);
 }
 
+/*
+	if(file == 0){
+		printf("file could not open\n\n");
+	} else {
+		int x;
+		while  ( ( x = fgetc( file ) ) != EOF )
+		{
+				printf( "%c", x );
+		}
+		fclose(file);
+	}
+}
+
+*/
 
 
 void print_graph(graph *g) {
